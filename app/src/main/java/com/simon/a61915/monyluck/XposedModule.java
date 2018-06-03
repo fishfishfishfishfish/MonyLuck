5ac569e0e1c325bca6418b0480ec5240486235cc
61915 <619156737@qq.com>
2018-06-03 13:08:14 +0800

    获得时间，开始修改view

--- app/src/main/java/com/simon/a61915/monyluck/XposedModule.java
+++ app/src/main/java/com/simon/a61915/monyluck/XposedModule.java
@@ -3,7 +3,13 @@ package com.simon.a61915.monyluck;
 import android.app.Application;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.StringReader;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
@@ -11,21 +17,28 @@ import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
+import static android.content.Context.MODE_PRIVATE;
+
 public class XposedModule implements IXposedHookLoadPackage {
+    private static int MODE = MODE_PRIVATE;
+    private static String SENDTIME_SP = "saveSendTime";
+    private static SharedPreferences SP;
     public void handleLoadPackage(final XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
         XposedBridge.log("Loaded app: " + lpparam.packageName);
         XposedHelpers.findAndHookMethod(Application.class, "attach", Context.class, new XC_MethodHook() {
             @Override
             protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                ClassLoader cl = ((Context)param.args[0]).getClassLoader();
+                Context ctx = (Context)param.args[0];
+                ClassLoader cl = ctx.getClassLoader();
+                SP = ctx.getSharedPreferences(SENDTIME_SP, MODE);
                 XposedBridge.log("finding process:"+lpparam.processName+",context:"+param.args[0]);
-//                hookRecivingTime(cl, lpparam.processName);
+                hookRecivingTime(cl, lpparam.processName);
                 hookSendingTime(cl, lpparam.processName);
                 super.afterHookedMethod(param);
             }
         });
     }
-    public static void hookSendingTime(final ClassLoader cl, String processName)throws Throwable{
+    private static void hookSendingTime(final ClassLoader cl, String processName)throws Throwable{
         XposedBridge.log("finding sdk.e in " + processName);
         try {
             XposedHelpers.findAndHookMethod("com.tencent.mm.bu.h", cl, "insert",
@@ -35,12 +48,16 @@ public class XposedModule implements IXposedHookLoadPackage {
                     Object tableName = param.args[0];
                     Object fieldName = param.args[1];
                     ContentValues cv = (ContentValues) param.args[2];
-                    XposedBridge.log("DBInfo-" + "table name: " + tableName + "\n");
-                    XposedBridge.log("DBInfo-" + "field name: " + fieldName + "\n");
-                    for (String key : cv.keySet()) {
-                        XposedBridge.log("DBInfo-" + "key=" + key + ", value=" + cv.get(key) + "\n");
+                    if("AppMessage".equals(tableName) || "msgId".equals(fieldName)){
+                        XposedBridge.log("WXField-" + "table name: " + tableName + "\n");
+                        XposedBridge.log("WXField-" + "field name: " + fieldName + "\n");
+                        String content = cv.get("xml").toString();
+                        String sendId = parseSendId(content).substring(13);
+                        String sendTime = Long.toString(System.currentTimeMillis()/1000);
+                        saveCreateTime(SP, sendId, sendTime);
+                        XposedBridge.log("WXField-content:" + sendId);
+                        XposedBridge.log("WXField-createTime:" + sendTime);
                     }
-                    XposedBridge.log("------table name: " + tableName + "----" + "field name: " + fieldName + "--end-----\n");
                     super.afterHookedMethod(param);
                 }
             });
@@ -49,7 +66,7 @@ public class XposedModule implements IXposedHookLoadPackage {
         }
     }
 
-    public static void hookRecivingTime(final ClassLoader cl, String processName) throws Throwable{
+    private static void hookRecivingTime(final ClassLoader cl, String processName) throws Throwable{
         Class<?> hookclass = null;
         try{
             hookclass = cl.loadClass("com.tencent.mm.plugin.luckymoney.ui.i");
@@ -59,25 +76,49 @@ public class XposedModule implements IXposedHookLoadPackage {
                 protected void afterHookedMethod(MethodHookParam param) throws Throwable {
                     Object obj = param.getResult();
                     Object owJ = obj.getClass().getField("owJ").get(obj);
-                    Object oxe = obj.getClass().getField("oxe").get(obj);
                     Object oxf = obj.getClass().getField("oxf").get(obj);
-                    Object oxs = obj.getClass().getField("oxs").get(obj);
-                    Object oxt = obj.getClass().getField("oxt").get(obj);
-                    Object oxu = obj.getClass().getField("oxu").get(obj);
                     Object userName = obj.getClass().getField("userName").get(obj);
+                    String sendId = owJ.toString().substring(13);
+                    String revTime = oxf.toString();
 
-                    XposedBridge.log("WXField-" + "owJ:" + owJ.toString());
-                    XposedBridge.log("WXField-" + "oxe:" + oxe.toString());
-                    XposedBridge.log("WXField-" + "oxf:" + oxf.toString());
-                    XposedBridge.log("WXField-" + "oxs:" + oxs.toString());
-                    XposedBridge.log("WXField-" + "oxt:" + oxt.toString());
-                    XposedBridge.log("WXField-" + "oxu:" + oxu.toString());
-                    XposedBridge.log("WXField-" + "userName:" + userName.toString());
+                    XposedBridge.log("WXField-" + "sendId:" + sendId); // sendId
+                    XposedBridge.log("WXField-" + "revTime:" + revTime); // 领取时间
+                    XposedBridge.log("WXField-" + "sendTime:" + SP.getString(sendId, "error"));
                     super.afterHookedMethod(param);
                 }
             });
         }catch (Exception e){
             XposedBridge.log("finding ui.i errors: "+ Log.getStackTraceString(e));
         }
+    }
+
+    private static String parseSendId(String xmlData) throws Exception{
+        XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+        XmlPullParser parser = factory.newPullParser();
+        parser.setInput(new StringReader(xmlData));
+        int eventType = parser.getEventType();
+        String res = "";
+        while (eventType != XmlPullParser.END_DOCUMENT){
+            String nodeName = parser.getName();
+            switch (eventType){
+                case XmlPullParser.START_TAG:{
+                    if("paymsgid".equals(nodeName)){
+                        res = parser.nextText();
+                    }
+                    break;
+                }
+            }
+            if(!"".equals(res)){
+                break;
+            }
+            parser.next();
+            eventType = parser.getEventType();
+        }
+        return res;
+    }
+    private static void saveCreateTime(SharedPreferences SP, String sendId, String createTime){
+        SharedPreferences.Editor SPEdit = SP.edit();
+        SPEdit.putString(sendId, createTime);
+        SPEdit.apply();
     }
 }
